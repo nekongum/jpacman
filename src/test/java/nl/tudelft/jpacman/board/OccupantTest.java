ec1d2d2fa73cd515300ad7513a21dc824b70db0f
nekongum <harujung212@gmail.com>
2025-10-31 20:58:23 +0700

    Add OccupantTest and DirectionTest cases

--- src/test/java/nl/tudelft/jpacman/board/OccupantTest.java
+++ src/test/java/nl/tudelft/jpacman/board/OccupantTest.java
@@ -32,7 +32,7 @@ class OccupantTest {
     @Test
     void noStartSquare() {
         // Remove the following placeholder:
-        assertThat(unit).isNotNull();
+        assertThat(unit.hasSquare()).isFalse();
     }
 
     /**
@@ -41,8 +41,13 @@ class OccupantTest {
      */
     @Test
     void testOccupy() {
-        // Remove the following placeholder:
-        assertThat(unit).isNotNull();
+        Square square = new BasicSquare();
+
+        unit.occupy(square);
+
+        assertThat(unit.hasSquare()).isTrue();
+        assertThat(unit.getSquare()).isEqualTo(square);
+        assertThat(square.getOccupants()).contains(unit);
     }
 
     /**
@@ -51,7 +56,15 @@ class OccupantTest {
      */
     @Test
     void testReoccupy() {
-        // Remove the following placeholder:
-        assertThat(unit).isNotNull();
+        Square square1 = new BasicSquare();
+        Square square2 = new BasicSquare();
+
+        unit.occupy(square1); // first placement
+        unit.occupy(square2); // move to second square
+
+        assertThat(unit.hasSquare()).isTrue();
+        assertThat(unit.getSquare()).isEqualTo(square2);
+        assertThat(square2.getOccupants()).contains(unit);
+        assertThat(square1.getOccupants()).doesNotContain(unit);
     }
 }
